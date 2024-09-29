13fc39ceb2e029702ae6eafbaaef878f28a169f3
Nikita Filonov <nikita.filonov@dif.tech>
2024-09-29 21:59:10 +0300

    Update CreateCoursePage.tsx

--- src/Pages/Courses/CreateCoursePage.tsx
+++ src/Pages/Courses/CreateCoursePage.tsx
@@ -1,13 +1,28 @@
 import { MainLayout } from '../../Components/Layouts/MainLayout';
 import CreateCourseView from '../../Views/Courses/CreateCourseView';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { setCourse } from '../../Redux/Courses/CoursesSlice';
+import { INITIAL_COURSES } from '../../Redux/Courses/InitialState';
 
 type Params = {
   courseId: string;
 };
 
 const CreateCoursePage = () => {
+  const dispatch = useDispatch();
   const { courseId } = useParams<Params>();
+
+  useEffect(() => {
+    if (courseId !== 'create') {
+      return;
+    }
+
+    return () => {
+      dispatch(setCourse(INITIAL_COURSES.course));
+    };
+  }, []);
 
   return (
     <MainLayout>

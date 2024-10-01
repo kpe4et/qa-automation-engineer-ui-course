7f5fb878b567c5bbbb743f9c65eb64a175b391e1
Nikita Filonov <nikita.filonov@dif.tech>
2024-10-01 14:21:17 +0300

    CoursesListToolbarView

--- src/Views/Courses/CoursesListToolbarView.tsx
+++ src/Views/Courses/CoursesListToolbarView.tsx
@@ -2,11 +2,16 @@ import { BaseToolbarView } from '../../Components/Toolbar/BaseToolbarView';
 import AddIcon from '@mui/icons-material/Add';
 import { useAppNavigationService } from '../../Services/HookServices/AppNavigationServiceHook';
 import { AppRoutes } from '../../Services/Constants/Routing';
+import { useDispatch } from 'react-redux';
+import { setCourse } from '../../Redux/Courses/CoursesSlice';
+import { INITIAL_COURSES } from '../../Redux/Courses/InitialState';
 
 export const CoursesListToolbarView = () => {
+  const dispatch = useDispatch();
   const { onNavigate } = useAppNavigationService();
 
   const onCreateCourse = () => {
+    dispatch(setCourse(INITIAL_COURSES.course));
     onNavigate(AppRoutes.CreateCourse, { courseId: 'create' });
   };
 

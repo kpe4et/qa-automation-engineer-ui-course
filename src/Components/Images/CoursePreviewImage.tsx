bfb52e6bd51aa1d4c36822dadc03c5f3a7782820
Nikita Filonov <nikita.filonov@dif.tech>
2024-10-01 14:05:21 +0300

    Create CoursePreviewImage.tsx

---
+++ src/Components/Images/CoursePreviewImage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+
+type CoursePreviewImageProps = {
+  image: string;
+};
+
+export const CoursePreviewImage: FC<CoursePreviewImageProps> = ({ image }) => {
+  return (
+    <img src={image} alt={'Preview Image'} style={{ marginTop: 16, width: '100%', height: 300, borderRadius: 3 }} />
+  );
+};
